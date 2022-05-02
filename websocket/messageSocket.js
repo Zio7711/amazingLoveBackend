89fe4d4bb797bb1bd41c3f187cde2902de7bcc51
zio7711 <zi.ao.tan@outlook.com>
2022-05-02 22:51:08 +0800

    fix socket io sending message bug

--- websocket/messageSocket.js
+++ websocket/messageSocket.js
@@ -1,17 +1,17 @@
 export const listenToMessageSocket = (socket) => {
-  socket.on('join_room', (data) => {
+  socket.on("join_room", (data) => {
     socket.join(data);
-    console.log(data);
-    console.log('socket room', socket.rooms);
+    console.log("join room, couple id", data);
+    console.log("socket room", socket.rooms);
   });
 
-  socket.on('send_messages', (data) => {
-    console.log('send messages', data.couple);
-    socket.to(data.couple).emit('receive_messages', data);
+  socket.on("send_message", (data) => {
+    console.log("send message", data.couple);
+    socket.to(data.couple).emit("receive_messages", data);
   });
 
-  socket.on('message', (data) => {
-    console.log('socket id: ', socket.id);
-    socket.emit('refresh', 'refresh fetching');
+  socket.on("message", (data) => {
+    console.log("socket id: ", socket.id);
+    socket.emit("refresh", "refresh fetching");
   });
 };
